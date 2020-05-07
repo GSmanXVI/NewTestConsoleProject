f8fd95fdcaf23c40f256f2b3d42ee6dfe4930b27
Gleb Skripnikov <gspostmail@gmail.com>
2020-05-07 13:16:35 +0400

    Calc renamed to Calculator

--- NewTestConsoleProject/Program.cs
+++ NewTestConsoleProject/Program.cs
@@ -10,11 +10,11 @@ namespace NewTestConsoleProject
             Console.WriteLine("Let's begin!");
             Console.WriteLine("Enter first number...");
             Console.WriteLine("Enter second number...");
-            Console.WriteLine(Calc.Sum(3, 5));
+            Console.WriteLine(Calculator.Sum(3, 5));
         }
     }
 
-    static class Calc
+    static class Calculator
     {
         static public int Sum(int num1, int num2)
         {
