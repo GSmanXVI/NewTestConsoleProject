56e0afe66bf4e2ca99280010276475988d09a777
Gleb Skripnikov <gspostmail@gmail.com>
2020-05-07 14:18:48 +0400

    Mlutiply method added

--- NewTestConsoleProject/Program.cs
+++ NewTestConsoleProject/Program.cs
@@ -25,5 +25,10 @@ namespace NewTestConsoleProject
         {
             return num1 - num2;
         }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
     }
 }
