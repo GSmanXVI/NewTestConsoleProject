dfdef7a36bb1e484a6366440c093cb744bd262f9
Gleb Skripnikov <gspostmail@gmail.com>
2020-05-07 12:33:39 +0400

    Welcome messsage

--- NewTestConsoleProject/Program.cs
+++ NewTestConsoleProject/Program.cs
@@ -6,7 +6,7 @@ namespace NewTestConsoleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to our console app!");
         }
     }
 }
